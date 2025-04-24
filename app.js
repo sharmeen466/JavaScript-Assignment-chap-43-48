4f47ac37bb4376ef20fbc8dffed99294751db64f
sharmeen466 <sharmeennazeer2@gmail.com>
2025-04-24 21:58:08 +0500

    chap # 43-48

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ alert("Hi")
 function mobile(){
     alert("Thanks for purchasing a phone from us 😊")
 }
+
+const remove = document.getElementsByClassName("delete")
+const hide = document.getElementsByClassName("display")
+const index = document.getElementsByClassName("index")
+console.log(index);
+for(let i = 0; i < remove.length; i++){
+    remove[i].addEventListener("click", function(){
+        hide[i].classList.add("hidden")
+    })
+}
+
+
+let increase = document.getElementById("inc")
+let decrease = document.getElementById("dec")
+let counter = document.getElementById("container")
+let numbers = 1
+counter.innerHTML = numbers
+console.log(counter);
+
+increase.addEventListener("click", function(){
+    numbers++
+    counter.innerHTML = numbers
+})
+decrease.addEventListener("click", function(){
+    numbers--
+    counter.innerHTML = numbers
+})
